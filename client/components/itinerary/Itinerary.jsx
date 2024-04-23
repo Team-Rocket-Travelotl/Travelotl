a12269068dd4e079f0b1ca631816ae954535c3aa
tristanfrank <TRISTAN1FRANK@GMAIL.COM>
2024-04-22 23:30:48 -0400

    Started refactoring itinerary component

--- client/components/itinerary/Itinerary.jsx
+++ client/components/itinerary/Itinerary.jsx
@@ -1,17 +1,59 @@
 const Itinerary = ({ itinerary }) => {
   // console.log("Itinerary Component:", itinerary.itinerary.itinerary);
+  const dates = Object.keys(itinerary.itinerary)
+
+  const mappedDisplayEntries = dates.map(date => {
+    return (
+      <div className="day-entry" key={date}>
+        <h2 className='date'>{date}</h2>
+        <div className="day-details">
+        {times = Object.keys(date)}
+        {/* each key/value pair inside the date object is a time and suggestion */}
+        {times.map(time => (
+          <div className='activity-details' key={time}>
+            <h3 className="time-of-day">{time}</h3>
+            <p>Activity: {time.suggestion.activity}</p>
+            <p>Description: {time.suggestion.description}</p>
+            <p>Address: {time.suggestion.address}</p>
+          </div>
+        ))}
+        </div>
+      </div>
+    )
+  });
+
+  
+
+// const example = {
+//   date1: {
+//     time1: {
+//       suggestion1: {
+//         activity: 'activity',
+//         description: 'description',
+//         address: 'address'
+//       }
+//     },
+//     time2: {}
+//   },
+  
+//   date2: {
+
+//   }
+// }
+
+
   if (itinerary) return (
     <div id='itinerary-details'>
-      {Object.entries(itinerary.itinerary).map(([date, timesOfDay]) => (
+      {dates.map(date => (
         <div className="day-entry" key={date}>
           <h2 className='date'>{date}</h2>
           <div className="day-details">
-            {Object.entries(timesOfDay).map(([timeOfDay, suggestion]) => (
-              <div className='activity-details' key={timeOfDay}>
-                <h3 className="time-of-day">{timeOfDay}</h3>
-                <p>Activity: {suggestion.activity}</p>
-                <p>Description: {suggestion.description}</p>
-                <p>Address: {suggestion.address}</p>
+            {Object.keys(date).map(time => (
+              <div className='activity-details' key={time}>
+                <h3 className="time-of-day">{time}</h3>
+                <p>Activity: {time.suggestion.activity}</p>
+                <p>Description: {time.suggestion.description}</p>
+                <p>Address: {time.suggestion.address}</p>
               </div>
             ))}
           </div>
