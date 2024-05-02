4966d12170667d4ef6f0d5c94dc447397d13e9e3
Elizabeth Sinclair <elizabethsinc.12@gmail.com>
2024-05-01 19:31:38 -0700

    merging

---
+++ client/components/main/Main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <p>Let us plan the trip of your dreams...</p>
+      <Link to="/form" id="start">
+        Click here to get started...{" "}
+      </Link>
+    </div>
+  );
+};
+
+export default Main;
