92f1aa6e6744fdac14f5db689efc9f7212b2b6bd
tristanfrank <TRISTAN1FRANK@GMAIL.COM>
2024-05-04 15:34:15 -0600

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,11 @@ import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from './routes/userRoutes';
+import itineraryRoutes from './routes/itineraryRoutes';
+import googleAuthRoutes from './routes/googleAuthRoutes';
+
+
 //use environmental variables
 dotenv.config({ path: './config.env' });
 
@@ -28,9 +33,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.urlencoded({ extended: true })); //parse urlencoded bodies
 
-app.use("/api/users", require("./routes/userRoutes.ts"));
-app.use("/api/trip", require("./routes/itineraryRoutes.ts"));
-app.use('/google-login', require('./routes/googleAuthRoutes.ts'));
+app.use("/api/users", userRoutes);
+app.use("/api/trip", itineraryRoutes);
+app.use('/google-login', googleAuthRoutes);
 
 
 app.get("/", (req: Request, res: Response) => {
